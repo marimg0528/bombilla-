3588d0a6cbac5746cb19f7d9da15de9033c1e2c9
Marimg0528 <160878613+marimg0528@users.noreply.github.com>
2024-05-16 11:27:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,28 +21,25 @@ client1.on_publish = on_publish
 
 client1.connect(broker, port)
 
-URL_MOTOR_ON = "https://wokwi.com/api/simulations/launch?url=https://wokwi.com/arduino/libraries/DigitalWrite&code=%2F%2F%20Wokwi%20DigitalWrite%20example%0A%23include%20%3CDigitalWrite.h%3E%0A%0AVirtualPin%20LED%20%3D%20%7B12%2C%20OUTPUT%7D%3B%0A%0Avoid%20setup%28%29%20%7B%0A%20%20pinMode%28LED%2C%20OUTPUT%29%3B%0A%7D%0A%0Avoid%20loop%28%29%20%7B%0A%20%20digitalWrite%28LED%2C%20HIGH%29%3B%20%2F%2F%20turn%20the%20LED%20on%0A%20%20delay%281000%29%3B%20%2F%2F%20wait%20for%201%20second%0A%20%20digitalWrite%28LED%2C%20LOW%29%3B%20%2F%2F%20turn%20the%20LED%20off%0A%20%20delay%281000%29%3B%20%2F%2F%20wait%20for%201%20second%0A%7D"
+URL_BOMBILLA = "https://wokwi.com/projects/377066087789108225"
 
-URL_MOTOR_OFF = "https://wokwi.com/api/simulations/launch?url=https://wokwi.com/arduino/libraries/DigitalWrite&code=%2F%2F%20Wokwi%20DigitalWrite%20example%0A%23include%20%3CDigitalWrite.h%3E%0A%0AVirtualPin%20LED%20%3D%20%7B12%2C%20OUTPUT%7D%3B%0A%0Avoid%20setup%28%29%20%7B%0A%20%20pinMode%28LED%2C%20OUTPUT%29%3B%0A%7D%0A%0Avoid%20loop%28%29%20%7B%0A%20%20digitalWrite%28LED%2C%20LOW%29%3B%20%2F%2F%20turn%20the%20LED%20off%0A%7D"
-
-def control_motor(url):
+def encender_bombilla():
     try:
-        response = requests.get(url)
+        response = requests.get(URL_BOMBILLA)
         if response.status_code == 200:
-            st.success("¡Operación exitosa!")
+            st.success("¡La luz se ha encendido!")
         else:
-            st.error("Hubo un problema al intentar controlar el motor.")
+            st.error("Hubo un problema al intentar encender la bombilla.")
     except requests.exceptions.RequestException as e:
         st.error(f"Error de conexión: {e}")
 
-st.title("Control de Motor")
-st.write("Escribe 'hola' para encender el motor y 'adios' para apagarlo.")
+st.title("Control de Bombillo")
+st.write("Escribe algo en el siguiente cuadro y presiona el botón para encender la luz.")
 
 input_texto = st.text_input("Escribe aquí:")
-boton_control = st.button("Controlar Motor")
+boton_encender = st.button("Encender Bombilla")
 
-if boton_control:
-    if input_texto == "hola":
-        control_motor(URL_MOTOR_ON)
-    elif input_texto == "adios":
-        control_motor(URL_MOTOR_OFF)
+if (input_texto=="hola"):
+    client1.publish("MAR","{'led': 'Enciende'}",qos=0, retain=False)
+if (input_texto=="adios"):
+    client1.publish("MAR","{'led': 'Apaga'}",qos=0, retain=False)

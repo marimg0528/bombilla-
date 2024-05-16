ee726435eb0e57658ee3a5ddd16eae5c6f30c9c5
Marimg0528 <160878613+marimg0528@users.noreply.github.com>
2024-05-16 11:10:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
+import paho.mqtt.client as paho
+import time
+import json
 import streamlit as st
-import requests
+import cv2
+import numpy as np
+#from PIL import Image
+from PIL import Image as Image, ImageOps as ImagOps
+from keras.models import load_model
 
-# Definir la URL del emulador Wokwi para encender la bombilla
+
+broker="broker.mqttdashboard.com"
+port=1883
+client1= paho.Client("Intento1")
+client1.on_message = on_message
+client1.on_publish = on_publish
+client1.connect(broker,port)
+
+def on_publish(client,userdata,result):             #create function for callback
+    print("el dato ha sido publicado \n")
+    pass
+
+def on_message(client, userdata, message):
+    global message_received
+    time.sleep(2)
+    message_received=str(message.payload.decode("utf-8"))
+    st.write(message_received)
 URL_BOMBILLA = "https://wokwi.com/projects/377066087789108225"
 
 # Función para enviar la señal y encender la bombilla
@@ -9,19 +32,20 @@ def encender_bombilla():
     try:
         response = requests.get(URL_BOMBILLA)
         if response.status_code == 200:
-            st.success("¡La bombilla se ha encendido!")
+            st.success("¡La luz se ha encendido!")
         else:
             st.error("Hubo un problema al intentar encender la bombilla.")
     except requests.exceptions.RequestException as e:
         st.error(f"Error de conexión: {e}")
 
 # Configurar la aplicación de Streamlit
-st.title("Control de Bombilla Emulada")
-st.write("Escribe algo en el siguiente cuadro y presiona el botón para encender la bombilla.")
+st.title("Control de Bombillo")
+st.write("Escribe algo en el siguiente cuadro y presiona el botón para encender la luz.")
 
 input_texto = st.text_input("Escribe aquí:")
 boton_encender = st.button("Encender Bombilla")
 
-# Manejar la acción del botón
-if boton_encender:
-    encender_bombilla()
+if (input_texto=="hola"):
+    client1.publish("MAR","{'led': 'Enciende'}",qos=0, retain=False)
+if (input_texto=="adios"):
+    client1.publish("MAR","{'led': 'Apaga'}",qos=0, retain=False)
